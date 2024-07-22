9cd024bfa217d1f00176e0c0892e744924e75b54
CleanPegasus <arunk609@gmail.com>
2024-07-22 17:21:21 +0530

    ✨ Implemented EVm memory

--- src/memory.rs
+++ src/memory.rs
@@ -1,8 +1,9 @@
 
 
 struct Memory {
-  memory: Vec<i32> 
+  memory: Vec<u8> 
 }
+
 
 impl Memory {
   fn new() -> Self {
@@ -12,15 +13,39 @@ impl Memory {
     }
   }
 
-  fn access(&self, access: usize, size: usize) -> i32 {
-    unimplemented!()
+  fn access(&self, access: usize, size: usize) -> Vec<u8> {
+    self.memory[access..access+size].to_vec()
+
   }
 
-  fn load(&self, offset: usize) -> i32 {
-    unimplemented!()
+  fn load(&self, offset: usize) -> Vec<u8> {
+    self.access(offset, 32)
   }
 
-  fn store(&mut self, offset: usize, value: i32) {
-    unimplemented!()
+  fn store(&mut self, offset: usize, value: Vec<u8>) -> usize {
+
+    let mut memory_expansion_cost = 0;
+
+    if self.memory.len() <= offset + value.len() {
+      let mut expansion_size = 0;
+
+      if self.memory.len() == 0 {
+        expansion_size = 32;
+        self.memory = vec![0x00; 32];
+      }
+
+      if self.memory.len() < offset + value.len() {
+        expansion_size += offset + value.len() - self.memory.len();
+        let new_size = self.memory.len() + expansion_size;
+        self.memory.resize(new_size, 0x00);
+      }
+
+      memory_expansion_cost = expansion_size.pow(2);
+    }
+
+    self.memory[offset..value.len()].copy_from_slice(&value);
+    
+    memory_expansion_cost
+
   }
 }
--- src/stack.rs
+++ src/stack.rs
@@ -1,23 +1,45 @@
+use std::fmt::{Display, Result, Formatter};
+
 static MAX_STACK_SIZE: usize = 1024;
 struct Stack {
-    items: Vec<i32> // TODO: Create a custom hex type
+    items: Vec<u8> // TODO: Create a custom hex type
 }
   
 impl Stack {
     fn new() -> Self {
-        let items: Vec<i32> = Vec::new();
+        let items: Vec<u8> = Vec::new();
         Self {
             items
         }
     }
 
-    fn push(&mut self, value: i32) {
+    fn push(&mut self, value: u8) {
         assert!(self.items.len() <= MAX_STACK_SIZE - 1, "Stack Overflow");
         self.items.push(value);
     }
 
-    fn pop(&mut self, n:i32) {
-        assert!(self.items.len() > n as usize, "Stack Overflow");
+    fn pop(&mut self, n: usize) {
+        assert!(self.items.len() > n, "Stack Underflow");
         (0..n).map(|_| self.items.pop());
     }
+}
+
+
+impl Display for Stack {
+  fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+      let len = self.items.len();
+      for (index, value) in self.items.iter().rev().enumerate() {
+          if index == 0 {
+              write!(f, "{} <-- top", value)?;
+          } else if index == len - 1 {
+              write!(f, "{} <-- bottom", value)?;
+          } else {
+              write!(f, "{}", value)?;
+          }
+          if index < len - 1 {
+              write!(f, ", ")?;
+          }
+      }
+      Ok(())
+  }
 }
