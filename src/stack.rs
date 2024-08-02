1b74f530ed83d11304fd9266d603f9a1de4adfc9
CleanPegasus <arunk609@gmail.com>
2024-08-02 11:26:35 +0530

    🐛 updated stack to use U256

--- src/stack.rs
+++ src/stack.rs
@@ -1,35 +1,45 @@
-use std::fmt::{Display, Result, Formatter};
+use std::fmt::{Display, Formatter};
+use primitive_types::U256;
 
 static MAX_STACK_SIZE: usize = 1024;
 pub struct Stack {
-    items: Vec<u8>
+    items: Vec<U256>
 }
   
 impl Stack {
     pub fn new() -> Self {
-        let items: Vec<u8> = Vec::new();
-        Self {
-            items
-        }
+        Self { items: Vec::new() }
     }
 
     /// Push value to the top of the stack
-    pub fn push(&mut self, value: u8) {
-        assert!(self.items.len() <= MAX_STACK_SIZE - 1, "Stack Overflow");
-        self.items.push(value);
+    pub fn push(&mut self, value: U256) -> Result<(), &'static str> {
+        if self.items.len() < MAX_STACK_SIZE {
+            self.items.push(value);
+            Ok(())
+        } else {
+            Err("Stack overflow")
+        }
     }
 
     /// Pop value from the top of the stack
-    pub fn pop(&mut self, n: usize) -> Vec<u8> {
-        assert!(self.items.len() > n, "Stack Underflow");
-        let popped_values = (0..n).map(|_| self.items.pop().unwrap()).collect();
-        popped_values
+    pub fn pop(&mut self) -> Result<U256, &'static str> {
+        self.items.pop().ok_or("Stack Underflow")
+    }
+
+    /// stack size
+    pub fn size(&self) -> usize {
+        self.items.len()
+    }
+
+    /// Peek the stack
+    pub fn peek(&self) -> Result<&U256, &'static str> {
+        self.items.last().ok_or("Stack Empty")
     }
 }
 
 /// Display the stack
 impl Display for Stack {
-  fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
       let len = self.items.len();
       for (index, value) in self.items.iter().rev().enumerate() {
           if index == 0 {
