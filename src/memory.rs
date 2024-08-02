6ba2c34995d8a06eb7b50df6211749e4bdb7881d
CleanPegasus <arunk609@gmail.com>
2024-08-02 12:05:26 +0530

    🐛 updated memory to use U256

--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,8 @@
+use primitive_types::U256;
 
 
 pub struct Memory {
-  memory: Vec<u8> 
+  memory: Vec<U256> 
 }
 
 
@@ -14,30 +15,31 @@ impl Memory {
   }
 
   /// fetch arbitrary sized data
-  pub fn access(&self, access: usize, size: usize) -> Vec<u8> {
+  pub fn access(&self, access: usize, size: usize) -> Vec<U256> {
     self.memory[access..access+size].to_vec()
 
   }
 
   /// fetch 32 byte data at offset
-  pub fn load(&self, offset: usize) -> Vec<u8> {
+  pub fn load(&self, offset: usize) -> Vec<U256> {
     self.access(offset, 32)
   }
 
   /// Store memory and return the gas cost
-  pub fn store(&mut self, offset: usize, value: Vec<u8>) -> usize {
+  pub fn store(&mut self, offset: usize, value: Vec<U256>) -> usize {
     let mut memory_expansion_cost = 0;
 
+    // Check if the memory needs to be expanded
     if self.memory.len() <= offset + value.len() {
       let mut expansion_size = 0;
       if self.memory.len() == 0 {
         expansion_size = 32;
-        self.memory = vec![0x00; 32];
+        self.memory = vec![U256::zero()];
       }
       if self.memory.len() < offset + value.len() {
         expansion_size += offset + value.len() - self.memory.len();
         let new_size = self.memory.len() + expansion_size;
-        self.memory.resize(new_size, 0x00);
+        self.memory.resize(new_size, U256::zero());
       }
 
       memory_expansion_cost = expansion_size.pow(2);
